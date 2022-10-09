da48d106a001e915ca769cf19b27d0fea441cc70
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-09 20:48:20 +0530

    First Commit

---
+++ C2TC_proj1/src/FirstProgram.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @author MEGHA
+ *
+ */
+public class FirstProgram {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");	// TODO Auto-generated method stub
+
+	}
+
+}
