4a85a7dab6f4f84ef38a746402d4f563eeeeea87
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-16 16:29:42 +0530

    First Commit

---
+++ C2TC_proj1/src/TernaryOperator.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author MEGHA
+ *
+ */
+public class TernaryOperator {
+	public static void main(String[] args) {
+		int x=10;
+		x=(10==x)? 1:0;
+		System.out.println(x);
+		
+
+	}
+
+}
