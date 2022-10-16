a34e15adb8d5bbd647e95c5c6fb45ff70eab453b
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-16 19:04:54 +0530

    First Commit

---
+++ C2TC_proj1/src/ForLoop.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author MEGHA
+ *
+ */
+public class ForLoop {
+	public static void main(String[] args) {
+		for(int i=1;i<1000;i++)
+		{
+			System.out.println("Value of i:");
+		System.out.println(i);
+		}
+	}
+}
