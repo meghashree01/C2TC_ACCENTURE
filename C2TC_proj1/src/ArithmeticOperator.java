b1ba873c5468b330a198a471f9414a85e0ff588b
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-16 16:26:29 +0530

    First Commit

--- C2TC_proj1/src/ArithmeticOperator.java
+++ C2TC_proj1/src/ArithmeticOperator.java
@@ -8,10 +8,10 @@
  */
 public class ArithmeticOperator {
 	public static void main(String[] args) {
-		int x=10,y=20;
-		System.out.println(x+y);
-		System.out.println(--x);
-		System.out.println(++y);
+			int x=10,y=20;
+				System.out.println(x+y);
+				System.out.println(--x);
+				System.out.println(++y);
+			
 	}
-
 }
