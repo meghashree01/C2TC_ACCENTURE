46c1a0a878942a05f9f036242f2be26d8ce30b6e
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-16 16:33:32 +0530

    First Commit

---
+++ C2TC_proj1/src/AssignmentOperator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author MEGHA
+ *
+ */
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		int x=4;
+		x+=5;
+		//x-=5;
+		//x*=5;
+		//x/=4;
+		//x=x%5;
+		System.out.println(x);
+
+	}
+
+}
