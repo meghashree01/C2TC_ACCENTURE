1943352313171e5bd1df3d787946d2092c9439e9
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-16 18:50:01 +0530

    First Commit

---
+++ C2TC_proj1/src/IfElse.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author MEGHA
+ *
+ */
+public class IfElse {
+	public static void main(String[] args) {
+	int x=6;
+	if(x>5)
+	{
+		System.out.println("X is greater than 5");
+	}
+	else
+	{
+		System.out.println("X is smaller tha 5"); 
+	}
+
+	}
+
+}
