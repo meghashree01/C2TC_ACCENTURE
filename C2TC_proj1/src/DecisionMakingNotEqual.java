2e1f3c941eb25cc8905ecf6261a44ce0cad23ee6
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-16 18:32:33 +0530

    First Commit

---
+++ C2TC_proj1/src/DecisionMakingNotEqual.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author MEGHA
+ *
+ */
+public class DecisionMakingNotEqual {
+	public static void main(String[] args) {
+		int x=6;
+		if(x!=5) {
+			System.out.println("Value of x is not 5");
+		}
+		else {
+			System.out.println("Value of x ix 5");
+		}
+	}
+
+}
