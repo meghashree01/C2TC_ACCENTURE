45b8e0be31561df1e3f8af9823d358ef96838969
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-16 18:39:15 +0530

    First Commit

---
+++ C2TC_proj1/src/NotLessThanOrEqualTo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author MEGHA
+ *
+ */
+public class NotLessThanOrEqualTo {
+	public static void main(String[] args) {
+		int x=10;
+		int y=11;
+		if(!(x<y)||(x==y))
+		{
+			System.out.println("Condition is true");
+		}
+		else
+		{
+			System.out.println("Condition is false");
+		}
+	}
+
+}
