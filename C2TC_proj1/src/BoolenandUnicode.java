aca9155218896f65d8d6a0fd765923d369d280f2
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-16 14:36:51 +0530

    First Commit

---
+++ C2TC_proj1/src/BoolenandUnicode.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author MEGHA
+ *
+ */
+public class BoolenandUnicode {
+	public static void main(String[] args) {
+		boolean var=false;
+		System.out.println(var);
+		char var1='\u00A7';
+		System.out.println(var1);
+
+	}
+
+}
