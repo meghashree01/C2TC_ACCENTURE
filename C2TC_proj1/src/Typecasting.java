191631e10d8bf5893dac482c52812b6b6927a5ca
MEGHA <MEGHA@LAPTOP-HE61RBOL>
2022-10-16 14:44:38 +0530

    First Commit

---
+++ C2TC_proj1/src/Typecasting.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author MEGHA
+ *
+ */
+public class Typecasting {
+	public static void main(String[] args) {
+		
+		float f = 10.532f;
+		long l = (long)f;
+		double d = f;
+		byte b =12;
+		short s = b; 
+		int a = s;
+		System.out.println(s);
+	}
+
+}
